87cf7aadd5c570d25e29ce0b5a6816d36ba8d709
Kartik Mishra <kartik26nov2004@gmail.com>
2026-02-14 03:19:43 +0530

    config fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/auth/:path*',
-        destination: `${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/api/auth/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
